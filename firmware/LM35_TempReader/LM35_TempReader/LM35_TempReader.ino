8b248319e16cea618abecad73262e55afdfe0355
hamsterxt309 <hieunguyenxt309@gmail.com>
2026-04-14 16:16:11 +0700

    feat: doi dinh dang Serial sang JSON

--- firmware/LM35_TempReader/LM35_TempReader/LM35_TempReader.ino
+++ firmware/LM35_TempReader/LM35_TempReader/LM35_TempReader.ino
@@ -3,20 +3,25 @@
  * Mon: Mang Cam Bien - Tuan 03
  */
 
-int adcValues[2]; float nhietDo[2]; char chuoi[30];
+int adcValues[2];
+float nhietDo[2];
+char chuoi[50];
 
 void setup() {
   Serial.begin(9600);
   delay(100);
 }
 
-
 void loop() {
   adcValues[0] = analogRead(A0);
   nhietDo[0] = (adcValues[0] * 500.0) / 1023.0;
+
   adcValues[1] = analogRead(A1);
   nhietDo[1] = (adcValues[1] * 500.0) / 1023.0;
-  sprintf(chuoi, "%d,%d\n", (int)nhietDo[0], (int)nhietDo[1]);
+
+  // Xuất JSON
+  sprintf(chuoi, "{\"A0\":%d,\"A1\":%d}\n", (int)nhietDo[0], (int)nhietDo[1]);
+
   Serial.print(chuoi);
   delay(100);
 }
